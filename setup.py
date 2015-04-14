3f068ca7e3c6f2deb4b954d792f45d6d159c6e5b
jiasir <jiasir@icloud.com>
2015-04-14 23:41:55 +0800

    Add warper

---
+++ setup.py
@@ -0,0 +1,27 @@
+import sys
+import os
+import shutil
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print("playback now needs setuptools in order to build. Install it using"
+          " your package manager (usually python-setuptools) or via pip (pip"
+          " install setuptools).")
+    sys.exit(1)
+
+setup(name='playback',
+      version='0.0.1',
+      description='Deny IPs from Queues',
+      author='jiasir',
+      author_email='jiasir@icloud.com',
+      url='https://github.com/jiasir/playback/',
+      license='MIT License',
+      install_requires=['pika == 0.9.8'],
+      packages=find_packages(),
+      scripts=[
+          'bin/playback',
+      ],
+      data_files=[],)
+os.mkdir('/etc/playback')
+shutil.copy('playback/playback.conf', '/etc/playback/playback.conf')
