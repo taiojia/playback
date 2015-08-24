d23b9834458c3388c28878438917be95273b46fb
jiasir <jiasir@icloud.com>
2015-08-24 20:17:15 +0800

    Add openjdk for gocd

---
+++ gocd/openjdk.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/jiasir/playback/libs/command"
+
+func main() {
+	if err := command.Command("sudo", "apt-get", "install", "-y", "openjdk-7-jdk"); err != nil {
+	panic(err)
+	}
+}
