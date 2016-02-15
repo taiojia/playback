9043b12122b192203aab59783a8f1023742456a1
jiasir <taio@outlook.com>
2016-02-15 16:04:40 +0800

    Remove configuration

--- playback/env.py
+++ playback/env.py
@@ -4,16 +4,14 @@ from playback import config
 from fabric.api import *
 
 
-config_file = config.Config('/etc/playback/playback.yaml')
-conf = config_file.get_config()
-
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
 
 group.add_argument('--prepare-host', help='Prepare the OpenStack environment', action='store_true', default=False, dest='prepare')
 group.add_argument('--gen-pass', help='Generate the password', action='store_true',default=False, dest='genpass')
 group.add_argument('--cmd', help='Run command line on the target host', action='store', dest='cmd')
-parser.add_argument('--hosts', help='hosts which to apply', action='store', dest='hosts')
+parser.add_argument('--user', help='The target user', action='store', dest='user')
+parser.add_argument('--hosts', help='The target address', action='store', dest='hosts')
 args = parser.parse_args()
 
 
@@ -23,7 +21,7 @@ def main():
         os.system('openssl rand -hex 10')
     if args.prepare:
         from playback import prepare_host   
-        remote = prepare_host.PrepareHost(user=conf['user'], hosts=args.hosts.split(','))
+        remote = prepare_host.PrepareHost(user=args.user, hosts=args.hosts.split(','))
 
         # host networking
         execute(remote.setup_external_interface)
@@ -36,7 +34,7 @@ def main():
 
     if args.cmd:
         from playback import cmd
-        remote = cmd.Cmd(user=conf['user'], hosts=args.hosts.split(','))
+        remote = cmd.Cmd(user=args.user, hosts=args.hosts.split(','))
         execute(remote.cmd, args.cmd)
 
 if __name__ == '__main__':
