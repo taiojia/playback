d6991563f80dc5574adc16016925abe3162e8efd
jiasir <taio@outlook.com>
2016-02-16 18:26:19 +0800

    Add MySQL HA

---
+++ playback/mysql_manage.py
@@ -0,0 +1,27 @@
+from fabric.api import *
+
+
+class MysqlManage(object):
+    """Manage Galera Cluster for MySQL"""
+
+    def __init__(self, user, hosts, parallel=True):
+        self.user = user
+        self.hosts = hosts
+        self.parallel = parallel
+        env.user = self.user
+        env.hosts = self.hosts
+        env.parallel = self.parallel
+
+    @serial
+    def _start_wsrep_new_cluster(self):
+        sudo('service mysql start --wsrep-new-cluster')
+
+    def _start_mysql(self):
+        sudo('service mysql start')
+
+    def _stop_mysql(self):
+        sudo('service mysql stop')
+
+    def _change_root_password(self, pwd):
+        sudo('mysqladmin -uroot password %s' % pwd)
+
