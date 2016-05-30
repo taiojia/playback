85f8c14290d738ca6de8d9fbe7fd2426c8b90a9c
jiasir <taio@outlook.com>
2016-05-30 17:56:38 +0800

    Refactor parser

--- playback/glance.py
+++ playback/glance.py
@@ -10,91 +10,6 @@ from playback.cli import cli_description
 from playback import __version__
 from playback.glance_conf import conf_glance_api_conf, conf_glance_registry_conf
 
-parser = argparse.ArgumentParser(description=cli_description+'this command used for provision Glance')
-parser.add_argument('-v', '--version',
-                   action='version',
-                   version=__version__)
-parser.add_argument('--user', 
-                    help='the target user', 
-                    action='store', 
-                    default='ubuntu', 
-                    dest='user')
-parser.add_argument('--hosts', 
-                    help='the target address', 
-                    action='store', 
-                    dest='hosts')
-
-subparsers = parser.add_subparsers(dest="subparser_name")
-create_glance_db = subparsers.add_parser('create-glance-db',
-                                   help='create the glance database')
-create_glance_db.add_argument('--root-db-pass', 
-                              help='the openstack database root passowrd',
-                              action='store', 
-                              default=None, 
-                              dest='root_db_pass')
-create_glance_db.add_argument('--glance-db-pass', 
-                              help='glance db passowrd',
-                              action='store', 
-                              default=None, 
-                              dest='glance_db_pass')
-
-create_service_credentials = subparsers.add_parser('create-service-credentials',
-                                                   help='create the glance service credentials')
-create_service_credentials.add_argument('--os-password',
-                                        help='the password for admin user',
-                                        action='store',
-                                        default=None,
-                                        dest='os_password')
-create_service_credentials.add_argument('--os-auth-url',
-                                        help='keystone endpoint url e.g. http://CONTROLLER_VIP:35357/v3',
-                                        action='store',
-                                        default=None,
-                                        dest='os_auth_url')
-create_service_credentials.add_argument('--glance-pass',
-                                        help='passowrd for glance user',
-                                        action='store',
-                                        default=None,
-                                        dest='glance_pass')
-create_service_credentials.add_argument('--endpoint',
-                                        help='public, internal and admin endpoint for glance service e.g. http://CONTROLLER_VIP:9292',
-                                        action='store',
-                                        default=None,
-                                        dest='endpoint')
-
-install = subparsers.add_parser('install', help='install glance(default store: swift)')
-install.add_argument('--connection',
-                    help='mysql database connection string e.g. mysql+pymysql://glance:GLANCE_PASS@CONTROLLER_VIP/glance',
-                    action='store',
-                    default=None,
-                    dest='connection')
-install.add_argument('--auth-uri',
-                    help='keystone internal endpoint e.g. http://CONTROLLER_VIP:5000',
-                    action='store',
-                    default=None,
-                    dest='auth_uri')
-install.add_argument('--auth-url',
-                    help='keystone admin endpoint e.g. http://CONTROLLER_VIP:35357',
-                    action='store',
-                    default=None,
-                    dest='auth_url')
-install.add_argument('--glance-pass',
-                    help='passowrd for glance user',
-                    action='store',
-                    default=None,
-                    dest='glance_pass')
-install.add_argument('--swift-store-auth-address',
-                    help='the address where the Swift authentication service is listening e.g. http://CONTROLLER_VIP:5000/v2.0/',
-                    action='store',
-                    default=None,
-                    dest='swift_store_auth_address')
-install.add_argument('--populate',
-                    help='populate the glance database',
-                    action='store_true',
-                    default=False,
-                    dest='populate')
-
-args = parser.parse_args()
-
 class Glance(Task):
     def __init__(self, user, hosts=None, parallel=True, *args, **kwargs):
         super(Glance, self).__init__(*args, **kwargs)
@@ -182,33 +97,144 @@ class Glance(Task):
         print red(env.host_string + ' | Remove the SQLite database file')
         sudo('rm -f /var/lib/glance/glance.sqlite')
 
-def main():
+def make_target(user, hosts):
+    """
+    The deployment instance
+    param::
+    user: str
+    hosts: list
+    """
     try:
-        target = Glance(user=args.user, hosts=args.hosts.split(','))
+        target = Glance(user, hosts)
     except AttributeError:
         print red('No hosts found. Please using --hosts param.')
-        parser.print_help()
         sys.exit(1)
 
-    if args.subparser_name == 'create-glance-db':
-        execute(target._create_glance_db, 
-                args.root_db_pass, 
-                args.glance_db_pass)
-    if args.subparser_name == 'create-service-credentials':
-        execute(target._create_service_credentials, 
-                args.os_password, 
-                args.os_auth_url, 
-                args.glance_pass, 
-                args.endpoint)
-    if args.subparser_name == 'install':
-        execute(target._install_glance, 
-                args.connection, 
-                args.auth_uri, 
-                args.auth_url, 
-                args.glance_pass,
-                args.swift_store_auth_address)
+    return target
 
-    disconnect_all()
+def create_glance_db(args):
+    target = make_target(args.user, args.hosts.split(','))
+    execute(target._create_glance_db, 
+            args.root_db_pass, 
+            args.glance_db_pass)
+
+def create_service_credentials(args):
+    target = make_target(args.user, args.hosts.split(','))
+    execute(target._create_service_credentials, 
+            args.os_password, 
+            args.os_auth_url, 
+            args.glance_pass, 
+            args.endpoint)
+
+def install(args):
+    target = make_target(args.user, args.hosts.split(','))
+    execute(target._install_glance, 
+            args.connection, 
+            args.auth_uri, 
+            args.auth_url, 
+            args.glance_pass,
+            args.swift_store_auth_address)
+def parser():
+    p = argparse.ArgumentParser(prog='playback-glance', description=cli_description+'this command used for provision Glance')
+    p.add_argument('-v', '--version', action='version', version=__version__)
+    p.add_argument('--user', help='the target user', action='store', default='ubuntu', dest='user')
+    p.add_argument('--hosts', help='the target address', action='store', dest='hosts')
+
+    s = p.add_subparsers(dest="subparser_name")
+
+    def create_glance_db_f(args):
+        create_glance_db(args)
+    create_glance_db_parser = s.add_parser('create-glance-db', help='create the glance database')
+    create_glance_db_parser.add_argument('--root-db-pass', 
+                                          help='the openstack database root passowrd',
+                                          action='store', 
+                                          default=None, 
+                                          dest='root_db_pass')
+    create_glance_db_parser.add_argument('--glance-db-pass', 
+                                          help='glance db passowrd',
+                                          action='store', 
+                                          default=None, 
+                                          dest='glance_db_pass')
+    create_glance_db_parser.set_defaults(func=create_glance_db)
+
+    def create_service_credentials_f(args):
+        create_service_credentials(args)
+    create_service_credentials_parser = s.add_parser('create-service-credentials',
+                                                   help='create the glance service credentials')
+    create_service_credentials_parser.add_argument('--os-password',
+                                                    help='the password for admin user',
+                                                    action='store',
+                                                    default=None,
+                                                    dest='os_password')
+    create_service_credentials_parser.add_argument('--os-auth-url',
+                                                    help='keystone endpoint url e.g. http://CONTROLLER_VIP:35357/v3',
+                                                    action='store',
+                                                    default=None,
+                                                    dest='os_auth_url')
+    create_service_credentials_parser.add_argument('--glance-pass',
+                                                    help='passowrd for glance user',
+                                                    action='store',
+                                                    default=None,
+                                                    dest='glance_pass')
+    create_service_credentials_parser.add_argument('--endpoint',
+                                                    help='public, internal and admin endpoint for glance service e.g. http://CONTROLLER_VIP:9292',
+                                                    action='store',
+                                                    default=None,
+                                                    dest='endpoint')
+    create_service_credentials_parser.set_defaults(func=create_service_credentials)
+
+    def install_f(args):
+        install(args)
+    install_parser = s.add_parser('install', help='install glance(default store: swift)')
+    install_parser.add_argument('--connection',
+                        help='mysql database connection string e.g. mysql+pymysql://glance:GLANCE_PASS@CONTROLLER_VIP/glance',
+                        action='store',
+                        default=None,
+                        dest='connection')
+    install_parser.add_argument('--auth-uri',
+                        help='keystone internal endpoint e.g. http://CONTROLLER_VIP:5000',
+                        action='store',
+                        default=None,
+                        dest='auth_uri')
+    install_parser.add_argument('--auth-url',
+                        help='keystone admin endpoint e.g. http://CONTROLLER_VIP:35357',
+                        action='store',
+                        default=None,
+                        dest='auth_url')
+    install_parser.add_argument('--glance-pass',
+                        help='passowrd for glance user',
+                        action='store',
+                        default=None,
+                        dest='glance_pass')
+    install_parser.add_argument('--swift-store-auth-address',
+                        help='the address where the Swift authentication service is listening e.g. http://CONTROLLER_VIP:5000/v2.0/',
+                        action='store',
+                        default=None,
+                        dest='swift_store_auth_address')
+    install_parser.add_argument('--populate',
+                        help='populate the glance database',
+                        action='store_true',
+                        default=False,
+                        dest='populate')
+    install_parser.set_defaults(func=install_f)
+
+    return p
+
+def main():
+    p = parser()
+    args = p.parse_args()
+    if not hasattr(args, 'func'):
+        p.print_help()
+    else:
+        # XXX on Python 3.3 we get 'args has no func' rather than short help.
+        try:
+            args.func(args)
+            disconnect_all()
+            return 0
+        except Exception as e:
+            sys.stderr.write(e.message)
+            sys.exit(1)
+    return 1
 
 if __name__ == '__main__':
     main()
