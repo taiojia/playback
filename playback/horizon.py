4c0dd459827ecab6b53f15952c2ec8fa6cf4b583
jiasir <taio@outlook.com>
2016-05-31 14:55:45 +0800

    Refactor parser and compatible with windows

--- playback/horizon.py
+++ playback/horizon.py
@@ -10,40 +10,6 @@ from playback.cli import cli_description
 from playback import __version__
 from playback.horizon_conf import conf_local_settings_py
 
-parser = argparse.ArgumentParser(description=cli_description+'this command used for provision Horizon')
-parser.add_argument('-v', '--version',
-                   action='version',
-                   version=__version__)
-parser.add_argument('--user', 
-                    help='the target user', 
-                    action='store', 
-                    default='ubuntu', 
-                    dest='user')
-parser.add_argument('--hosts', 
-                    help='the target address', 
-                    action='store', 
-                    dest='hosts')
-subparsers = parser.add_subparsers(dest='subparser_name')
-install = subparsers.add_parser('install',
-                                help='install horizon')
-install.add_argument('--openstack-host',
-                    help='configure the dashboard to use OpenStack services on the controller node e.g. CONTROLLER_VIP',
-                    action='store',
-                    default=None,
-                    dest='openstack_host')
-install.add_argument('--memcache',
-                    help='django memcache e.g. CONTROLLER1:11211',
-                    action='store',
-                    default=None,
-                    dest='memcache')
-install.add_argument('--time-zone',
-                    help='the timezone of the server. This should correspond with the timezone of your entire OpenStack installation e.g. Asia/Shanghai',
-                    action='store',
-                    default=None,
-                    dest='time_zone')
-
-args = parser.parse_args()
-
 class Horizon(Task):
     def __init__(self, user, hosts=None, parallel=True, *args, **kwargs):
         super(Horizon, self).__init__(*args, **kwargs)
@@ -54,6 +20,7 @@ class Horizon(Task):
         env.hosts = self.hosts
         env.parallel = self.parallel
 
+    @runs_once
     def _install(self, openstack_host, memcache, time_zone):
         print red(env.host_string + ' | Install the packages')
         sudo('apt-get update')
@@ -71,21 +38,71 @@ class Horizon(Task):
                                        'time_zone': time_zone})
         os.remove('tmp_local_settings_py_' + env.host_string)
 
-
-
-def main():
+def install(args):
     try:
         target = Horizon(user=args.user, hosts=args.hosts.split(','))
     except AttributeError:
         print red('No hosts found. Please using --hosts param.')
-        parser.print_help()
         sys.exit(1)
 
-    if args.subparser_name == 'install':
-        execute(target._install, 
-                args.openstack_host, 
-                args.memcache, 
-                args.time_zone)
+    execute(target._install, 
+            args.openstack_host, 
+            args.memcache, 
+            args.time_zone)
+
+def parser():
+    p = argparse.ArgumentParser(prog='playback-horizon', description=cli_description+'this command used for provision Horizon')
+    p.add_argument('-v', '--version',
+                    action='version',
+                    version=__version__)
+    p.add_argument('--user', 
+                    help='the target user', 
+                    action='store', 
+                    default='ubuntu', 
+                    dest='user')
+    p.add_argument('--hosts', 
+                    help='the target address', 
+                    action='store', 
+                    dest='hosts')
+    s = p.add_subparsers(dest='subparser_name')
+
+    def install_f(args):
+        install(args)
+    install_parser = s.add_parser('install', help='install horizon')
+    install_parser.add_argument('--openstack-host',
+                                help='configure the dashboard to use OpenStack services on the controller node e.g. CONTROLLER_VIP',
+                                action='store',
+                                default=None,
+                                dest='openstack_host')
+    install_parser.add_argument('--memcache',
+                                help='django memcache e.g. CONTROLLER1:11211',
+                                action='store',
+                                default=None,
+                                dest='memcache')
+    install_parser.add_argument('--time-zone',
+                                help='the timezone of the server. This should correspond with the timezone of your entire OpenStack installation e.g. Asia/Shanghai',
+                                action='store',
+                                default=None,
+                                dest='time_zone')
+    install_parser.set_defaults(func=install_f)
+
+    return p
+
+def main():
+    p = parser()
+    args = p.parse_args()
+    if not hasattr(args, 'func'):
+        p.print_help()
+    else:
+        # XXX on Python 3.3 we get 'args has no func' rather than short help.
+        try:
+            args.func(args)
+            disconnect_all()
+            return 0
+        except Exception as e:
+            sys.stderr.write(e.message)
+            sys.exit(1)
+    return 1
 
 if __name__ == '__main__':
     main()
