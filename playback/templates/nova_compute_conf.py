36f5293090f4b72125ae2b16cb4eb94fa0320d9a
jiasir <jiasir@icloud.com>
2016-06-25 04:24:36 +0800

    Update rbd_uuid for compute

--- playback/templates/nova_compute_conf.py
+++ playback/templates/nova_compute_conf.py
@@ -6,7 +6,7 @@ images_type = rbd
 images_rbd_pool = vms
 images_rbd_ceph_conf = /etc/ceph/ceph.conf
 rbd_user = cinder
-rbd_secret_uuid = {{ rbd_secret_uuid }}
+rbd_uuid = {{ rbd_uuid }}
 disk_cachemodes="network=writeback"
 live_migration_flag="VIR_MIGRATE_UNDEFINE_SOURCE,VIR_MIGRATE_PEER2PEER,VIR_MIGRATE_LIVE,VIR_MIGRATE_PERSIST_DEST,VIR_MIGRATE_TUNNELLED"
 """
