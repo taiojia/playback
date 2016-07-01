e047e7bd00719d4e22d7551a0e53595253398145
jiasir <jiasir@icloud.com>
2016-07-01 15:21:44 +0800

    Add warn_only for systemd

--- playback/mysql_manage.py
+++ playback/mysql_manage.py
@@ -8,7 +8,7 @@ class MysqlManage(common.Common):
     def _start_wsrep_new_cluster(self):
         sudo('service mysql start --wsrep-new-cluster')
         if self._release() == 'xenial':
-            result = sudo('systemctl start mysql --wsrep-new-cluster')
+            result = sudo('systemctl start mysql --wsrep-new-cluster', warn_only=True)
             if result.failed:
                 sudo('service mysql start --wsrep-new-cluster')
 
