09e9f2144c6f94cae2fca14dd1a3737756bcdf2f
jiasir <jiasir@icloud.com>
2016-07-01 15:28:29 +0800

    Fix systemd

--- playback/mysql_manage.py
+++ playback/mysql_manage.py
@@ -6,21 +6,24 @@ class MysqlManage(common.Common):
     """Manage Galera Cluster for MySQL"""
 
     def _start_wsrep_new_cluster(self):
-        sudo('service mysql start --wsrep-new-cluster')
         if self._release() == 'xenial':
             result = sudo('systemctl start mysql --wsrep-new-cluster', warn_only=True)
             if result.failed:
                 sudo('service mysql start --wsrep-new-cluster')
+        else:
+            sudo('service mysql start --wsrep-new-cluster')
 
-    def _start_mysql(self):
-        sudo('service mysql start')
+    def _start_mysql(self):     
         if self._release() == 'xenial':
             sudo('systemctl start mysql')
+        else:
+            sudo('service mysql start')
 
     def _stop_mysql(self):
-        sudo('service mysql stop')
         if self._release() == 'xenial':
             sudo('systemctl stop mysql')
+        else:
+            sudo('service mysql stop')
 
     def _change_root_password(self, pwd):
         sudo('mysqladmin -uroot password %s' % pwd)
