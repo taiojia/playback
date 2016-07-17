6e7e9e1003be37bbb5851ce84d31835eabd1bbd9
jiasir <jiasir@icloud.com>
2016-07-17 11:26:11 +0800

    Move to fastforward

--- playback/common.py
+++ playback/common.py
@@ -23,26 +23,3 @@ class Common(Task):
         release = sudo('lsb_release -cs')
         return release
 
-def parser(des):
-    """
-    Setup common parser
-    return: parser and subparsers
-    """
-    p = argparse.ArgumentParser(prog='nova-deploy', description=cli_description + des)
-    p.add_argument('-v', '--version',
-                    action='version',
-                    version=__version__)
-    p.add_argument('--user', 
-                    help='the target user', 
-                    action='store', 
-                    default='ubuntu', 
-                    dest='user')
-    p.add_argument('--hosts', 
-                    help='the target address', 
-                    action='store', 
-                    dest='hosts')
-    p.add_argument('-i', '--key-filename', help='referencing file paths to SSH key files to try when connecting', action='store', dest='key_filename', default=None)
-    p.add_argument('--password', help='the password used by the SSH layer when connecting to remote hosts', action='store', dest='password', default=None)
-
-    s = p.add_subparsers(dest="subparser_name")
-    return p, s
