a3b149e6959be7742830c35fc481492d81feb777
jiasir <jiasir@icloud.com>
2016-07-18 13:56:23 +0800

    Update doc

--- playback/config.py
+++ playback/config.py
@@ -1,13 +1,18 @@
 import yaml
 
-
 class Config(object):
-    """Parse the playback configuration"""
+    """
+    Parse the playback configuration
+    """
     def __init__(self, path):
         self.path = path
 
     def get_config(self):
-        """Return the configuration of path to the dict or list"""
+        """
+        Read config file
+        
+        :returns: the configuration of path to the dict or list
+        """
         with open(self.path) as f:
             return yaml.safe_load(f)
 
