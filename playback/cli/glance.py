097cd333ccd607b60119cde0bf021bb4b624ef96
jiasir <jiasir@icloud.com>
2016-12-23 17:32:06 +0800

    Glance parser refactoring

--- playback/cli/glance.py
+++ playback/cli/glance.py
@@ -1,5 +1,7 @@
+import logging, sys
 from playback.cli.cliutil import priority
 from playback.api import Glance
+from cliff.command import Command
 
 def make_target(user, hosts, key_filename, password):
     try:
@@ -39,100 +41,121 @@ def install(args):
 
 @priority(15)
 def make(parser):
-    """provison Glance with HA"""
+    """DEPRECATED
+    provison Glance with HA"""
     s = parser.add_subparsers(
         title='commands',
         metavar='COMMAND',
         help='description',
         )
 
-    def create_glance_db_f(args):
-        create_glance_db(args)
-    create_glance_db_parser = s.add_parser('create-glance-db', help='create the glance database')
-    create_glance_db_parser.add_argument('--root-db-pass',
-                                          help='the openstack database root passowrd',
-                                          action='store',
-                                          default=None,
-                                          dest='root_db_pass')
-    create_glance_db_parser.add_argument('--glance-db-pass',
-                                          help='glance db passowrd',
-                                          action='store',
-                                          default=None,
-                                          dest='glance_db_pass')
-    create_glance_db_parser.set_defaults(func=create_glance_db)
+class CreateGlanceDB(Command):
+    """create the glance database"""
 
-    def create_service_credentials_f(args):
-        create_service_credentials(args)
-    create_service_credentials_parser = s.add_parser('create-service-credentials',
-                                                   help='create the glance service credentials')
-    create_service_credentials_parser.add_argument('--os-password',
-                                                    help='the password for admin user',
-                                                    action='store',
-                                                    default=None,
-                                                    dest='os_password')
-    create_service_credentials_parser.add_argument('--os-auth-url',
-                                                    help='keystone endpoint url e.g. http://CONTROLLER_VIP:35357/v3',
-                                                    action='store',
-                                                    default=None,
-                                                    dest='os_auth_url')
-    create_service_credentials_parser.add_argument('--glance-pass',
-                                                    help='passowrd for glance user',
-                                                    action='store',
-                                                    default=None,
-                                                    dest='glance_pass')
-    create_service_credentials_parser.add_argument('--public-endpoint',
-                                                    help='public endpoint for glance service e.g. http://CONTROLLER_VIP:9292',
-                                                    action='store',
-                                                    default=None,
-                                                    dest='public_endpoint')
-    create_service_credentials_parser.add_argument('--internal-endpoint',
-                                                    help='internal endpoint for glance service e.g. http://CONTROLLER_VIP:9292',
-                                                    action='store',
-                                                    default=None,
-                                                    dest='internal_endpoint')
-    create_service_credentials_parser.add_argument('--admin-endpoint',
-                                                    help='admin endpoint for glance service e.g. http://CONTROLLER_VIP:9292',
-                                                    action='store',
-                                                    default=None,
-                                                    dest='admin_endpoint')
-    create_service_credentials_parser.set_defaults(func=create_service_credentials)
+    log = logging.getLogger(__name__)
 
-    def install_f(args):
-        install(args)
-    install_parser = s.add_parser('install', help='install glance(default store: ceph)')
-    install_parser.add_argument('--connection',
-                        help='mysql database connection string e.g. mysql+pymysql://glance:GLANCE_PASS@CONTROLLER_VIP/glance',
-                        action='store',
-                        default=None,
-                        dest='connection')
-    install_parser.add_argument('--auth-uri',
-                        help='keystone internal endpoint e.g. http://CONTROLLER_VIP:5000',
-                        action='store',
-                        default=None,
-                        dest='auth_uri')
-    install_parser.add_argument('--auth-url',
-                        help='keystone admin endpoint e.g. http://CONTROLLER_VIP:35357',
-                        action='store',
-                        default=None,
-                        dest='auth_url')
-    install_parser.add_argument('--glance-pass',
-                        help='passowrd for glance user',
-                        action='store',
-                        default=None,
-                        dest='glance_pass')
-    install_parser.add_argument('--swift-store-auth-address',
-                        help='DEPRECATED the address where the Swift authentication service is listening e.g. http://CONTROLLER_VIP:5000/v3/',
-                        action='store',
-                        default='DEPRECATED_BY_PLAYBACK',
-                        dest='swift_store_auth_address')
-    install_parser.add_argument('--memcached-servers',
-                        help='memcached servers e.g. CONTROLLER1:11211,CONTROLLER2:11211',
-                        action='store',
-                        default=None,
-                        dest='memcached_servers')
-    install_parser.add_argument('--populate',
-                        help='populate the glance database',
-                        action='store_true',
-                        default=False,
-                        dest='populate')
-    install_parser.set_defaults(func=install_f)
+    def get_parser(self, prog_name):
+        parser = super(CreateGlanceDB, self).get_parser(prog_name)
+        parser.add_argument('--user',
+                            help='the username to connect to the remote host', action='store', default='ubuntu',
+                            dest='user')
+        parser.add_argument('--hosts',
+                            help='the remote host to connect to ', action='store', default=None, dest='hosts')
+        parser.add_argument('-i', '--key-filename',
+                            help='referencing file paths to SSH key files to try when connecting', action='store',
+                            dest='key_filename',
+                            default=None)
+        parser.add_argument('--password',
+                            help='the password used by the SSH layer when connecting to remote hosts', action='store',
+                            dest='password',
+                            default=None)
+        parser.add_argument('--root-db-pass', help='the openstack database root passowrd',
+                            action='store', default=None, dest='root_db_pass')
+        parser.add_argument('--glance-db-pass', help='glance db passowrd',
+                            action='store', default=None, dest='glance_db_pass')
+        return parser
+
+    def take_action(self, parsed_args):
+        create_glance_db(parsed_args)
+
+
+class CreateServiceCredentials(Command):
+    """create the glance service credentials"""
+
+    log = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        parser = super(CreateServiceCredentials, self).get_parser(prog_name)
+        parser.add_argument('--user',
+                            help='the username to connect to the remote host', action='store', default='ubuntu',
+                            dest='user')
+        parser.add_argument('--hosts',
+                            help='the remote host to connect to ', action='store', default=None, dest='hosts')
+        parser.add_argument('-i', '--key-filename',
+                            help='referencing file paths to SSH key files to try when connecting', action='store',
+                            dest='key_filename',
+                            default=None)
+        parser.add_argument('--password',
+                            help='the password used by the SSH layer when connecting to remote hosts', action='store',
+                            dest='password',
+                            default=None)
+        parser.add_argument('--os-password', help='the password for admin user',
+                            action='store', default=None, dest='os_password')
+        parser.add_argument('--os-auth-url', help='keystone endpoint url e.g. http://CONTROLLER_VIP:35357/v3',
+                            action='store', default=None, dest='os_auth_url')
+        parser.add_argument('--glance-pass', help='passowrd for glance user',
+                            action='store', default=None, dest='glance_pass')
+        parser.add_argument('--public-endpoint', help='public endpoint for glance service e.g. http://CONTROLLER_VIP:9292',
+                            action='store', default=None, dest='public_endpoint')
+        parser.add_argument('--internal-endpoint', help='internal endpoint for glance service e.g. http://CONTROLLER_VIP:9292',
+                            action='store', default=None, dest='internal_endpoint')
+        parser.add_argument('--admin-endpoint', help='admin endpoint for glance service e.g. http://CONTROLLER_VIP:9292',
+                            action='store', default=None, dest='admin_endpoint')
+        return parser
+
+    def take_action(self, parsed_args):
+        create_service_credentials(parsed_args)
+
+
+class Install(Command):
+    """install glance(default store: ceph)"""
+
+    log = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        parser = super(Install, self).get_parser(prog_name)
+        parser.add_argument('--user',
+                            help='the username to connect to the remote host',
+                            action='store', default='ubuntu', dest='user')
+        parser.add_argument('--hosts',
+                            help='the remote host to connect to ',
+                            action='store', default=None, dest='hosts')
+        parser.add_argument('-i', '--key-filename',
+                            help='referencing file paths to SSH key files to try when connecting',
+                            action='store', dest='key_filename', default=None)
+        parser.add_argument('--password',
+                            help='the password used by the SSH layer when connecting to remote hosts',
+                            action='store', dest='password', default=None)
+        parser.add_argument('--connection', help='mysql database connection string e.g. mysql+pymysql://glance:GLANCE_PASS@CONTROLLER_VIP/glance',
+                            action='store', default=None, dest='connection')
+        parser.add_argument('--auth-uri', help='keystone internal endpoint e.g. http://CONTROLLER_VIP:5000',
+                            action='store',default=None, dest='auth_uri')
+        parser.add_argument('--auth-url',
+                            help='keystone admin endpoint e.g. http://CONTROLLER_VIP:35357',
+                            action='store', default=None, dest='auth_url')
+        parser.add_argument('--glance-pass',
+                            help='passowrd for glance user',
+                            action='store', default=None, dest='glance_pass')
+        parser.add_argument('--swift-store-auth-address',
+                            help='DEPRECATED the address where the Swift authentication service is listening e.g. http://CONTROLLER_VIP:5000/v3/',
+                            action='store', default='DEPRECATED_BY_PLAYBACK', dest='swift_store_auth_address')
+        parser.add_argument('--memcached-servers',
+                            help='memcached servers e.g. CONTROLLER1:11211,CONTROLLER2:11211',
+                            action='store', default=None, dest='memcached_servers')
+        parser.add_argument('--populate',
+                            help='populate the glance database',
+                            action='store_true', default=False, dest='populate')
+        return parser
+
+    def take_action(self, parsed_args):
+        install(parsed_args)
