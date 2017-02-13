505fc834734424f71deba29b77b2701e80463672
jiasir <jiasir@icloud.com>
2017-02-13 10:23:48 +0800

    Rabbitmq parser refactoring

--- playback/cli/rabbitmq.py
+++ playback/cli/rabbitmq.py
@@ -1,53 +1,74 @@
 import sys
+import logging
 from playback.api import RabbitMq
-from playback.cli.cliutil import priority
+from cliff.command import Command
 
-def install_subparser(s):
-    install_parser = s.add_parser('install', help='install RabbitMQ HA')
-    install_parser.add_argument('--erlang-cookie', help='setup elang cookie',
-                                action='store', default=None, dest='erlang_cookie')
-    install_parser.add_argument('--rabbit-user', help='set rabbit user name',
-                                action='store', default=None, dest='rabbit_user')
-    install_parser.add_argument('--rabbit-pass', help='set rabbit password',
-                                action='store', default=None, dest='rabbit_pass')
-    return install_parser
-
-def join_cluster_subparser(s):
-    join_cluster_parser = s.add_parser('join-cluster', help='join the rabbit cluster')
-    join_cluster_parser.add_argument('--name', help='the joined name, e.g. rabbit@CONTROLLER1',
-                                    action='store', default=None, dest='name')
-    return join_cluster_parser
 
 def make_target(args):
     try:
-        target = RabbitMq(user=args.user, hosts=args.hosts.split(','), key_filename=args.key_filename, password=args.password)
+        target = RabbitMq(user=args.user, hosts=args.hosts.split(','), key_filename=args.key_filename,
+                          password=args.password)
     except AttributeError:
         sys.stderr.write('No hosts found. Please using --hosts param.')
         sys.exit(1)
     return target
 
+
 def install(args):
     target = make_target(args)
     target.install(args.erlang_cookie, args.rabbit_user, args.rabbit_pass)
+
 
 def join_cluster(args):
     target = make_target(args)
     target.join_cluster(args.name)
 
-@priority(13)
-def make(parser):
-    """provision RabbitMQ with HA"""
-    s = parser.add_subparsers(
-        title='commands',
-        metavar='COMMAND',
-        help='description',
-        )
-    def install_f(args):
-        install(args)
-    install_parser = install_subparser(s)
-    install_parser.set_defaults(func=install_f)
 
-    def join_cluster_f(args):
-        join_cluster(args)
-    join_cluster_parser = join_cluster_subparser(s)
-    join_cluster_parser.set_defaults(func=join_cluster_f)
+class Install(Command):
+    """install rabbitmq"""
+
+    log = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        parser = super(Install, self).get_parser(prog_name)
+        parser.add_argument('--user',
+                            help='the username to connect to the remote host',
+                            action='store', default='ubuntu', dest='user')
+        parser.add_argument('--hosts',
+                            help='the remote host to connect to ',
+                            action='store', default=None, dest='hosts')
+        parser.add_argument('-i', '--key-filename',
+                            help='referencing file paths to SSH key files to try when connecting',
+                            action='store', dest='key_filename', default=None)
+        parser.add_argument('--password',
+                            help='the password used by the SSH layer when connecting to remote hosts',
+                            action='store', dest='password', default=None)
+        parser.add_argument('--erlang-cookie',
+                            help='setup elang cookie',
+                            action='store', default=None, dest='erlang_cookie')
+        parser.add_argument('--rabbit-user',
+                            help='set rabbit user name',
+                            action='store', default=None, dest='rabbit_user')
+        parser.add_argument('--rabbit-pass',
+                            help='set rabbit password',
+                            action='store', default=None, dest='rabbit_pass')
+        return parser
+
+    def take_action(self, parsed_args):
+        install(parsed_args)
+
+
+class JoinCluster(Command):
+    """join the rabbit cluster"""
+
+    log = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        parser = super(JoinCluster, self).get_parser(prog_name)
+        parser.add_argument('--name',
+                            help='the joined name, e.g. rabbit@CONTROLLER1',
+                            action='store', default=None, dest='name')
+        return parser
+
+    def take_action(self, parsed_args):
+        join_cluster(parsed_args)
