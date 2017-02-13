e3e59c56aca0e79ee333a8a0db813ca47cb97dc2
jiasir <jiasir@icloud.com>
2017-02-13 10:48:59 +0800

    Swift proxy parser refactoring

--- playback/cli/swift.py
+++ playback/cli/swift.py
@@ -1,83 +1,8 @@
 import sys
-from playback.cli.cliutil import priority
+import logging
 from playback.api import Swift
+from cliff.command import Command
 
-def create_service_credentials_subparser(s):
-    create_service_credentials_parser = s.add_parser('create-service-credentials', help='create the swift service credentials')
-    create_service_credentials_parser.add_argument('--os-password',
-                                                    help='the password for admin user',
-                                                    action='store',
-                                                    default=None,
-                                                    dest='os_password')
-    create_service_credentials_parser.add_argument('--os-auth-url',
-                                                    help='keystone endpoint url e.g. http://CONTROLLER_VIP:35357/v3',
-                                                    action='store',
-                                                    default=None,
-                                                    dest='os_auth_url')
-    create_service_credentials_parser.add_argument('--swift-pass',
-                                                    help='password for swift user',
-                                                    action='store',
-                                                    default=None,
-                                                    dest='swift_pass')
-    create_service_credentials_parser.add_argument('--public-endpoint',
-                                                    help=r'public endpoint for swift service e.g. "http://CONTROLLER_VIP:8080/v1/AUTH_%%\(tenant_id\)s"',
-                                                    action='store',
-                                                    default=None,
-                                                    dest='public_endpoint')
-    create_service_credentials_parser.add_argument('--internal-endpoint',
-                                                    help=r'internal endpoint for swift service e.g. "http://CONTROLLER_VIP:8080/v1/AUTH_%%\(tenant_id\)s"',
-                                                    action='store',
-                                                    default=None,
-                                                    dest='internal_endpoint')
-    create_service_credentials_parser.add_argument('--admin-endpoint',
-                                                    help='admin endpoint for swift service e.g. http://CONTROLLER_VIP:8080/v1',
-                                                    action='store',
-                                                    default=None,
-                                                    dest='admin_endpoint')
-    return create_service_credentials_parser
-
-def install_subparser(s):
-    install_parser = s.add_parser('install',help='install swift proxy')
-    install_parser.add_argument('--auth-uri',
-                                help='keystone internal endpoint e.g. http://CONTROLLER_VIP:5000',
-                                action='store',
-                                default=None,
-                                dest='auth_uri')
-    install_parser.add_argument('--auth-url',
-                                help='keystone admin endpoint e.g. http://CONTROLLER_VIP:35357',
-                                action='store',
-                                default=None,
-                                dest='auth_url')
-    install_parser.add_argument('--swift-pass',
-                                help='password for swift user',
-                                action='store',
-                                default=None,
-                                dest='swift_pass')
-    install_parser.add_argument('--memcached-servers',
-                                help='memcache servers e.g. CONTROLLER1:11211,CONTROLLER2:11211',
-                                action='store',
-                                default=None,
-                                dest='memcached_servers')
-    install_parser.add_argument('--with-memcached',
-                                help='install memcached on remote server, if you have other memcached on the controller node, you can use --memcached-sersers',
-                                action='store_true',
-                                default=False,
-                                dest='with_memcached')
-    return install_parser
-
-def finalize_install_subparser(s):
-    finalize_install_parser = s.add_parser('finalize-install', help='finalize swift installation')
-    finalize_install_parser.add_argument('--swift-hash-path-suffix',
-                                        help='swift_hash_path_suffix and swift_hash_path_prefix are used as part of the hashing algorithm when determining data placement in the cluster. These values should remain secret and MUST NOT change once a cluster has been deployed',
-                                        action='store',
-                                        default=None,
-                                        dest='swift_hash_path_suffix')
-    finalize_install_parser.add_argument('--swift-hash-path-prefix',
-                                        help='swift_hash_path_suffix and swift_hash_path_prefix are used as part of the hashing algorithm when determining data placement in the cluster. These values should remain secret and MUST NOT change once a cluster has been deployed',
-                                        action='store',
-                                        default=None,
-                                        dest='swift_hash_path_prefix')
-    return finalize_install_parser
 
 def make_target(args):
     try:
@@ -112,26 +37,116 @@ def finalize_install(args):
             args.swift_hash_path_suffix,
             args.swift_hash_path_prefix)
 
-@priority(22)
-def make(parser):
-    """provison Swift Proxy service with HA"""
-    s = parser.add_subparsers(
-        title='commands',
-        metavar='COMMAND',
-        help='description',
-        )
 
-    def create_service_credentials_f(args):
-        create_service_credentials(args)
-    create_service_credentials_parser = create_service_credentials_subparser(s)
-    create_service_credentials_parser.set_defaults(func=create_service_credentials_f)
+class CreateServiceCredentials(Command):
+    """create the swift service credentials"""
 
-    def install_f(args):
-        install(args)
-    install_parser = install_subparser(s)
-    install_parser.set_defaults(func=install_f)
+    log = logging.getLogger(__name__)
 
-    def finalize_install_f(args):
-        finalize_install(args)
-    finalize_install_parser = finalize_install_subparser(s)
-    finalize_install_parser.set_defaults(func=finalize_install_f)
+    def get_parser(self, prog_name):
+        parser = super(CreateServiceCredentials, self).get_parser(prog_name)
+        parser.add_argument('--user',
+                            help='the username to connect to the remote host',
+                            action='store', default='ubuntu', dest='user')
+        parser.add_argument('--hosts',
+                            help='the remote host to connect to ',
+                            action='store', default=None, dest='hosts')
+        parser.add_argument('-i', '--key-filename',
+                            help='referencing file paths to SSH key files to try when connecting',
+                            action='store', dest='key_filename', default=None)
+        parser.add_argument('--password',
+                            help='the password used by the SSH layer when connecting to remote hosts',
+                            action='store', dest='password', default=None)
+        parser.add_argument('--os-password',
+                            help='the password for admin user',
+                            action='store', default=None, dest='os_password')
+        parser.add_argument('--os-auth-url',
+                            help='keystone endpoint url e.g. http://CONTROLLER_VIP:35357/v3',
+                            action='store', default=None, dest='os_auth_url')
+        parser.add_argument('--swift-pass',
+                            help='password for swift user',
+                            action='store', default=None, dest='swift_pass')
+        parser.add_argument('--public-endpoint',
+                            help=r'public endpoint for swift service e.g. "http://CONTROLLER_VIP:8080/v1/AUTH_%%\(tenant_id\)s"',
+                            action='store', default=None, dest='public_endpoint')
+        parser.add_argument('--internal-endpoint',
+                            help=r'internal endpoint for swift service e.g. "http://CONTROLLER_VIP:8080/v1/AUTH_%%\(tenant_id\)s"',
+                            action='store', default=None, dest='internal_endpoint')
+        parser.add_argument('--admin-endpoint',
+                            help='admin endpoint for swift service e.g. http://CONTROLLER_VIP:8080/v1',
+                            action='store', default=None, dest='admin_endpoint')
+        return parser
+
+    def take_action(self, parsed_args):
+        create_service_credentials(parsed_args)
+
+
+class Install(Command)
+    """install swift proxy"""
+
+    log = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        parser = super(Install, self).get_parser(prog_name)
+        parser.add_argument('--user',
+                            help='the username to connect to the remote host',
+                            action='store', default='ubuntu', dest='user')
+        parser.add_argument('--hosts',
+                            help='the remote host to connect to ',
+                            action='store', default=None, dest='hosts')
+        parser.add_argument('-i', '--key-filename',
+                            help='referencing file paths to SSH key files to try when connecting',
+                            action='store', dest='key_filename', default=None)
+        parser.add_argument('--password',
+                            help='the password used by the SSH layer when connecting to remote hosts',
+                            action='store', dest='password', default=None)
+        parser.add_argument('--auth-uri',
+                            help='keystone internal endpoint e.g. http://CONTROLLER_VIP:5000',
+                            action='store', default=None, dest='auth_uri')
+        parser.add_argument('--auth-url',
+                            help='keystone admin endpoint e.g. http://CONTROLLER_VIP:35357',
+                            action='store', default=None, dest='auth_url')
+        parser.add_argument('--swift-pass',
+                            help='password for swift user',
+                            action='store', default=None, dest='swift_pass')
+        parser.add_argument('--memcached-servers',
+                            help='memcache servers e.g. CONTROLLER1:11211,CONTROLLER2:11211',
+                            action='store', default=None, dest='memcached_servers')
+        parser.add_argument('--with-memcached',
+                            help='install memcached on remote server, if you have other memcached on the controller node, you can use --memcached-sersers',
+                            action='store_true', default=False, dest='with_memcached')
+        return parser
+
+    def take_action(self, parsed_args):
+        install(parsed_args)
+
+
+class InstallFinalize(Command):
+    """finalize swift installation"""
+
+    log = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        parser = super(InstallFinalize, self).get_parser(prog_name)
+        parser.add_argument('--user',
+                            help='the username to connect to the remote host',
+                            action='store', default='ubuntu', dest='user')
+        parser.add_argument('--hosts',
+                            help='the remote host to connect to ',
+                            action='store', default=None, dest='hosts')
+        parser.add_argument('-i', '--key-filename',
+                            help='referencing file paths to SSH key files to try when connecting',
+                            action='store', dest='key_filename', default=None)
+        parser.add_argument('--password',
+                            help='the password used by the SSH layer when connecting to remote hosts',
+                            action='store', dest='password', default=None)
+        parser.add_argument('--swift-hash-path-suffix',
+                            help='swift_hash_path_suffix and swift_hash_path_prefix are used as part of the hashing algorithm when determining data placement in the cluster. These values should remain secret and MUST NOT change once a cluster has been deployed',
+                            action='store', default=None, dest='swift_hash_path_suffix')
+        parser.add_argument('--swift-hash-path-prefix',
+                            help='swift_hash_path_suffix and swift_hash_path_prefix are used as part of the hashing algorithm when determining data placement in the cluster. These values should remain secret and MUST NOT change once a cluster has been deployed',
+                            action='store', default=None, dest='swift_hash_path_prefix')
+        return parser
+
+    def take_action(self, parsed_args):
+        finalize_install(parsed_args)
